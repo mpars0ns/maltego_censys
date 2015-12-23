fbdbd7c1fe5e8ff24bad5fb4751b56ec0d6ff34d
Mark Parsons <mark.parsons@gmail.com>
2015-12-22 21:51:03 -0500

    Added cert to ip lookup and the entity I created for the SSL Cert

---
+++ censys_cert_to_ip.py
@@ -0,0 +1,51 @@
+import requests
+import sys
+import json
+from MaltegoTransform import *
+
+
+def main():
+    mt = MaltegoTransform()
+    sha1 = sys.argv[1]
+    censys_uid = 'Enter_your_uid_here'
+    censys_secret = 'Enter_your_uid_here'
+    auth = (censys_uid, censys_secret)
+    query = {'query': '443.https.tls.certificate.parsed.fingerprint_sha1: {s}'.format(s=sha1), 'fields': ['ip',
+                                                                                                          'updated_at']}
+    try:
+        request = requests.post('https://www.censys.io/api/v1/search/ipv4', data=json.dumps(query), auth=auth)
+        if request.status_code == 200:
+            results = request.json()
+            if results['metadata']['count'] > 0:
+                for result in results['results']:
+                    if 'ip' in result:
+                        ip = result['ip']
+                        updated = result['updated_at'][0]
+                        newip = mt.addEntity("maltego.IPv4Address", ip)
+                        newip.addAdditionalFields("property.last_updated", "Last updated time", True, updated)
+                    else:
+                        mt.addUIMessage("Hmm there is info on the SSL Hash but no ip info :( sadness")
+
+            else:
+                mt.addUIMessage("No IP addresses found with this ssl cert")
+        if request.status_code == 400:
+            results = request.json()
+            mt.addUIMessage(str(results['error']))
+        if request.status_code == 429:
+            results = request.json()
+            mt.addUIMessage(str(results['error']))
+        if request.status_code == 404:
+            mt.addUIMessage("No IP addresses found with this ssl cert")
+        if request.status_code == 500:
+            mt.addUIMessage("There has been a server error!!!")
+        mt.returnOutput()
+
+    except requests.exceptions.RequestException as e:
+        mt.addUIMessage(str(e))
+        mt.returnOutput()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print "User aborted."
--- censys_ip_to_cert.py
+++ censys_ip_to_cert.py
@@ -1,6 +1,6 @@
 import requests
-import sys
 import json
+import sys
 from MaltegoTransform import *
 
 
@@ -8,27 +8,29 @@ def main():
     mt = MaltegoTransform()
     ip = sys.argv[1]
     censys_uid = 'Enter_your_uid_here'
-    censys_secret = 'Enter_your_censys_secret_here'
+    censys_secret = 'Enter_your_uid_here'
     auth = (censys_uid, censys_secret)
     query = {'query': 'ip: {ip}'.format(ip=ip), 'fields': ['443.https.tls.certificate.parsed.fingerprint_sha1',
-                                                              '443.https.tls.certificate.parsed.issuer_dn',
-                                                              '443.https.tls.certificate.parsed.subject_dn',
-                                                              'updated_at']}
+                                                           '443.https.tls.certificate.parsed.issuer_dn',
+                                                           '443.https.tls.certificate.parsed.subject_dn',
+                                                           'updated_at']}
     try:
         request = requests.post('https://www.censys.io/api/v1/search/ipv4', data=json.dumps(query), auth=auth)
         if request.status_code == 200:
             results = request.json()
             if results['metadata']['count'] > 0:
                 for result in results['results']:
-                    sha1 = result['443.https.tls.certificate.parsed.fingerprint_sha1'][0]
-                    issuer = result['443.https.tls.certificate.parsed.issuer_dn'][0]
-                    subject = result['443.https.tls.certificate.parsed.subject_dn'][0]
-                    updated = result['updated_at'][0]
-                    sslcert = mt.addEntity("censys.sslcertificate", sha1)
-                    sslcert.addAdditionalFields("property.issuer","Cert Issuer",True, issuer)
-                    sslcert.addAdditionalFields("property.subject","Cert Subject",True, subject)
-                    sslcert.addAdditionalFields("property.last_updated","Last updated time",True, updated)
-                mt.returnOutput()
+                    if '443.https.tls.certificate.parsed.fingerprint_sha1' in result:
+                        sha1 = result['443.https.tls.certificate.parsed.fingerprint_sha1'][0]
+                        issuer = result['443.https.tls.certificate.parsed.issuer_dn'][0]
+                        subject = result['443.https.tls.certificate.parsed.subject_dn'][0]
+                        updated = result['updated_at'][0]
+                        sslcert = mt.addEntity("censys.sslcertificate", sha1)
+                        sslcert.addAdditionalFields("property.issuer", "Cert Issuer", True, issuer)
+                        sslcert.addAdditionalFields("property.subject", "Cert Subject", True, subject)
+                        sslcert.addAdditionalFields("property.last_updated", "Last updated time", True, updated)
+                    else:
+                        mt.addUIMessage("Hmm there is info on the IP but not ssl :( ")
             else:
                 mt.addUIMessage("No SSL certs were found on this ip: {ip}".format(ip=ip))
         if request.status_code == 400:
@@ -41,9 +43,11 @@ def main():
             mt.addUIMessage("No SSL certs were found on this ip: {ip}".format(ip=ip))
         if request.status_code == 500:
             mt.addUIMessage("There has been a server error!!!")
+        mt.returnOutput()
 
     except requests.exceptions.RequestException as e:
         mt.addUIMessage(str(e))
+        mt.returnOutput()
 
 if __name__ == "__main__":
     try:
